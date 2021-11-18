26aee17fa09a03d4dbd1f5c06c5b8109dd13f154
Manuel <emmanuel.francois@onexsolucoes.com.br>
2021-11-18 10:10:07 -0300

    [ENCRYPT PASSWORD WITH BCRYPT]

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,47 +1,57 @@
- const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
- const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
 
-    name: {
-        type: String,
-        trim: true,
-        required : [true, 'Please add a Name'],
-        maxlength: 32
-    },
+   name: {
+       type: String,
+       trim: true,
+       required : [true, 'Please add a Name'],
+       maxlength: 32
+   },
 
-    email: {
-        type: String,
-        trim: true,
-        required : [true, 'Please add a E-mail'],
-        unique: true,
-        match: [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            'Please add a valid E-mail'
-        ]
+   email: {
+       type: String,
+       trim: true,
+       required : [true, 'Please add a E-mail'],
+       unique: true,
+       match: [
+           /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+           'Please add a valid E-mail'
+       ]
 
-    },
+   },
 
-    password: {
-        type: String,
-        trim: true,
-        required : [true, 'Please add a Password'],
-        minlength: [6, 'password must have at least six(6) characters'],
-        match: [
-            /^(?=.*\d)(?=.*[@#\-_$%^&+=§!\?])(?=.*[a-z])(?=.*[A-Z])[0-9A-Za-z@#\-_$%^&+=§!\?]+$/,
-            'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number and a special characters'
-        ]
-    },
+   password: {
+       type: String,
+       trim: true,
+       required : [true, 'Please add a Password'],
+       minlength: [6, 'password must have at least six(6) characters'],
+       match: [
+           /^(?=.*\d)(?=.*[@#\-_$%^&+=§!\?])(?=.*[a-z])(?=.*[A-Z])[0-9A-Za-z@#\-_$%^&+=§!\?]+$/,
+           'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number and a special characters'
+       ]
+   },
 
-    role: {
-        type: Number,
-        default: 0,
-   
-    },
+   role: {
+       type: Number,
+       default: 0,
+  
+   },
 
 
 
- }, {timestamps: true});
+}, {timestamps: true});
 
- 
 
- module.exports = mongoose.model("User", userSchema);
+
+// encrypting password before saving
+userSchema.pre('save', async function(next){
+
+   if(!this.isModified('password')){
+       next()
+   }
+   this.password = await bcrypt.hash(this.password, 10);
+});
+
+module.exports = mongoose.model("User", userSchema);
